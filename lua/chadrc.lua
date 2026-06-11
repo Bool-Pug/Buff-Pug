1d19441ddc0b9eff12a9be703a214fb455acfcab
Bool-Pug <jswwolf120@gmail.com>
2026-06-11 08:52:24 -0500

    Adding initial config

---
+++ lua/chadrc.lua
@@ -0,0 +1,58 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "mito-laser",
+  transparency = true,
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.nvdash = { load_on_startup = true }
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
+
+
+M.ui = {
+     cmp = {
+       lspkind_text = true,
+       style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+       format_colors = {
+         lsp = true,
+       },
+     },
+ 
+     telescope = { style = "bordered" }, -- borderless / bordered
+ 
+     statusline = {
+       enabled = true,
+       theme = "vscode", -- default/vscode/vscode_colored/minimal
+       -- default/round/block/arrow separators work only for default statusline theme
+       -- round and block will work for minimal theme only
+       separator_style = "arrow",
+       order = nil,
+       modules = nil,
+     },
+ 
+     -- lazyload it when there are 1+ buffers
+     tabufline = {
+       enabled = true,
+       lazyload = true,
+       treeOffsetFt = "NvimTree",
+       order = { "treeOffset", "buffers", "tabs", "btns" },
+       modules = nil,
+       bufwidth = 21,
+     },
+   }
+
+return M
