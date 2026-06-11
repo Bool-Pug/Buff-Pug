45190a733a5d61190f698ec6e483cec596b2896f
Bool-Pug <jswwolf120@gmail.com>
2026-06-11 10:57:13 -0500

    Added some better bindings for quick saving and quitting

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n"," w", ":w<enter>")
+map("n"," q", ":q<enter>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
