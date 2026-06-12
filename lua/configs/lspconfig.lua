7485819c0a9711b0b3176b149d26303e0fc886ae
Bool-Pug <jswwolf120@gmail.com>
2026-06-12 13:06:46 -0500

    Added LSPs, I think

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "pyright", "kotlin_language_server"}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
